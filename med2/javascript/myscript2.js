839119d4c899889272f40436b6f9373babefd7e9
herval20 <96063828+herval20@users.noreply.github.com>
2023-02-16 12:51:01 +0100

    Add files via upload

---
+++ med2/javascript/myscript2.js
@@ -0,0 +1,24 @@
+
+// Der prompt Befehl erzeugt eine Eingabebox. 
+// prompt("Wie alt bist du?");
+
+// Text steht immer unter Anführungszeichen. 
+// Text und Variablen kann man mit dem Pluszeichen kombinieren
+alert("Du bist " + prompt ("Wie alt bist du") + " Jahre alt ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
